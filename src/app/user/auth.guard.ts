fbea37e7ba6e82cf673ca1f56a40d835b5e4a344
Shamil <shamil.badalloffsh@gmail.com>
2020-03-25 06:34:15 +0100

    Add: auth to main page redirecting to sign in page

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { SnackService } from '../services/snack.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private snack: SnackService,
   ) {}
 
   async canActivate(
@@ -16,7 +18,10 @@ export class AuthGuard implements CanActivate {
   ): Promise<boolean> {
     const user = await this.afAuth.auth.currentUser;
     const isLoggedIn = !!user;
-
+    
+    if (!isLoggedIn) {
+      this.snack.authError();
+    }
     return isLoggedIn;
   }
   
