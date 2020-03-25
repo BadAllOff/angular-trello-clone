432e4397aa9c5ca13abd557277bddc464d60d2bd
Shamil <shamil.badalloffsh@gmail.com>
2020-03-25 06:24:25 +0100

    Add: guard logic

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
+  constructor(
+    private afAuth: AngularFireAuth
+  ) {}
+
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const user = await this.afAuth.auth.currentUser;
+    const isLoggedIn = !!user;
+
+    return isLoggedIn;
   }
   
 }
