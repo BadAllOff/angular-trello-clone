3a2bd0cf00651215ef2d6b683a44c36a8ae136f9
Shamil <shamil.badalloffsh@gmail.com>
2020-03-29 04:40:12 +0200

    Add: routing tto customer module and SEO service

---
+++ src/app/services/seo.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Title, Meta } from "@angular/platform-browser";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeoService {
+
+  constructor() { }
+}
